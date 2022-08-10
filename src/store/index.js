027635fe288108a7380c17ee0c8ddb8cfdc76c04
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2022-08-10 22:54:15 +0530

    init

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    api: `http://localhost:4000`,
+    user: {
+      guest: true,
+      username: "",
+      email: "",
+    }
   },
   getters: {
   },
   mutations: {
+    changeUser(state, user){
+      state.user = user;
+    }
   },
   actions: {
   },
---
+++ src/views/SignupView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Signup</h1>
+    <form @submit="submission">
+      <input
+        required
+        v-model="username"
+        type="text"
+        placeholder="Enter Username"
+      />
+      <input v-model="email" required type="email" placeholder="Email" />
+      <input
+        required
+        :type="show ? 'text' : 'password'"
+        placeholder="Enter Password"
+        v-model="password"
+      />
+      <button type="button" @click="changeShow">👀</button>
+      <input type="submit" value="Submit" />
+      <p :style="{ color: error ? 'red' : 'green' }">{{ message }}</p>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      show: false,
+      error: false,
+      message: "",
+    };
+  },
+  methods: {
+    changeShow() {
+      this.show = !this.show;
+    },
+    async submission() {
+      const d = await fetch(`${this.$store.state.api}/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        }),
+      });
+      const data = await d.json();
+      console.log(data);
+      if (data.error) {
+        this.error = true;
+      } else {
+        this.error = false;
+      }
+      this.message = data.message;
+    },
+  },
+};
+</script>
