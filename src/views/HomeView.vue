fcd7cd263cb0396ad0f46d0aa2c5f435976fa10f
Samik-Tuteja <samiktuteja2008@gmail.com>
2022-08-12 10:55:15 +0530

    Styled login, signup and home(partially)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,58 +9,71 @@
       <img class="bg-transparent -rotate-12 mt-28 pb-40" src="../assets/sneakersmain.png" alt="nahi aaya">
     </div>
     <!-- Hero section ends -->
-    <!-- Section 2 starts -->
-    <div class="mt-12">
-      <!-- section 2 div1 -->
-        <div class="flex justify-center">
-          <i class="fa fa-angle-left mt-32 ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 mt-12">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right mt-32 ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+    <!-- Section 2 Starts -->
+    <div>
+      <div>
+        <div>
+          <img class="w-full" src="../assets/sneakers2.png" alt="">
+          <div>
+            <h1>Lorem</h1>
+            <h1>Lorem ipsum dolor.</h1>
+          </div>
         </div>
-        <!-- section 2 div2 -->
-                <div class="flex justify-center">
-          <i class="fa fa-angle-left mt-32 ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 mt-12">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right mt-32 ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
-        </div>
-        <!-- section 2 div 3 -->
-                <div class="flex justify-center">
-          <i class="fa fa-angle-left mt-32 ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 mt-12">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right mt-32 ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
-        </div>
+      </div>
     </div>
     <!-- Section 2 ends -->
+    <!-- Section 3 starts -->
+    <div class="mt-12">
+      <!-- section 3 div1 -->
+        <div class="flex justify-center bg-black text-white">
+          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
+            <div class="w-1/2 ml-16 my-auto">
+                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
+                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
+                <div class="flex justify-center mt-12">
+                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
+                  <a class="text-white hover:text-black py-1.5 px-8 rounded-3xl ml-8 text-xl border-solid border-2 border-white bg-black hover:bg-white duration-300" href="#">Buy</a>
+                </div>
+            </div>
+            <div>
+              <img class="py-20 pl-12" src="../assets/sneakersmain.png" alt="">
+            </div>
+         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+        </div>
+        <!-- section 3 div2 -->
+        <div class="flex justify-center">
+          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
+            <div class="w-1/2 ml-16 mt-12">
+                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
+                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
+                <div class="flex justify-center mt-12">
+                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
+                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
+                </div>
+            </div>
+            <div>
+              <img src="../assets/sneakersmain.png" alt="">
+            </div>
+         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+        </div>
+        <!-- section 3 div 3 -->
+                <div class="flex justify-center">
+          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
+            <div class="w-1/2 ml-16 mt-12">
+                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
+                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
+                <div class="flex justify-center mt-12">
+                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
+                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
+                </div>
+            </div>
+            <div>
+              <img src="../assets/sneakersmain.png" alt="">
+            </div>
+         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+        </div>
+    </div>
+    <!-- Section 3 ends -->
   </div>
 </template>
 
