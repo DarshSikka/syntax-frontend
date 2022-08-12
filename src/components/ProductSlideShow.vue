9a95994a5692d638d978e4d69c00063b859e0654
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2022-08-12 16:03:18 +0530

    slideshow

---
+++ src/components/ProductSlideShow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center bg-black text-white">
+    <i
+      class="fa fa-angle-left my-auto ml-24 cursor-pointer"
+      style="font-size: 42px"
+      @click="$emit('previous')"
+    ></i>
+    <div class="w-1/2 ml-16 my-auto">
+      <h1 class="text-4xl">{{ title }}</h1>
+      <h1 class="text-2xl mt-8">
+        {{ description }}
+      </h1>
+      <div class="flex justify-center mt-12">
+        <h1 class="flex items-center text-2xl font-semibold py-1.5">
+          ₹{{ price }}
+        </h1>
+        <a
+          class="text-white hover:text-black py-1.5 px-8 rounded-3xl ml-8 text-xl border-solid border-2 border-white bg-black hover:bg-white duration-300"
+          :href="`/product/${product_id}`"
+          >Buy</a
+        >
+      </div>
+    </div>
+    <div>
+      <img class="py-20 pl-12" :src="photo" alt="" />
+    </div>
+    <i
+      class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer"
+      style="font-size: 42px"
+      @click="$emit('next')"
+    ></i>
+  </div>
+</template>
+<script>
+export default {
+  props: ["photo", "title", "description", "price", "product_id"],
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,153 +2,284 @@
   <div style="background-color: rgb(247, 247, 247)">
     <!-- Hero section starts -->
     <div class="flex justify-around">
-      <div style="font-family: 'Raleway', sans-serif;" class="my-auto w-1/3">
+      <div style="font-family: 'Raleway', sans-serif" class="my-auto w-1/3">
         <h1 class="text-8xl">SNEAKERS</h1>
         <h1 class="text-3xl">Lorem ipsum dolor sit amet consectetur</h1>
       </div>
-      <img class="bg-transparent -rotate-12 mt-28 pb-40" src="../assets/sneakersmain.png" alt="nahi aaya">
+      <img
+        class="bg-transparent -rotate-12 mt-28 pb-40"
+        src="../assets/sneakersmain.png"
+        alt="nahi aaya"
+      />
     </div>
     <!-- Hero section ends -->
-      <!-- section 2 div1 -->
-        <div class="flex justify-center bg-black text-white">
-          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 my-auto">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="text-white hover:text-black py-1.5 px-8 rounded-3xl ml-8 text-xl border-solid border-2 border-white bg-black hover:bg-white duration-300" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img class="py-20 pl-12" src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+    <!-- section 2 div1 -->
+    <ProductSlideShow
+      :title="product.title"
+      :description="product.description"
+      :photo="product.image_url"
+      :price="product.price"
+      :product_id="product.product_id"
+      @next="next"
+      @previous="prev"
+    />
+    <!-- Section 2 ends -->
+    <!-- Section 3 starts -->
+    <div>
+      <h1 class="text-left ml-32 text-4xl mt-32 mb-12">Lorem Ipsum</h1>
+      <!-- section 3 div1 -->
+      <div class="flex">
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
         </div>
-        <!-- section 2 div2 -->
-        <div class="flex justify-center">
-          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 mt-12">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img class="bg-transparent" src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
         </div>
-        <!-- section 2 div 3 -->
-                <div class="flex justify-center">
-          <i class="fa fa-angle-left my-auto ml-24 cursor-pointer" style="font-size:42px"></i>
-            <div class="w-1/2 ml-16 mt-12">
-                <h1 class="text-4xl">Lorem Ipsum Dolar.</h1>
-                <h1 class="text-2xl mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quibusdam expedita beatae error quis quas! Cumque placeat incidunt culpa nobis veritatis qui voluptates laborum accusamus quis alias! Quis, quidem quaerat.</h1>
-                <div class="flex justify-center mt-12">
-                  <h1 class="flex items-center text-2xl font-semibold py-1.5">₹20,000</h1>
-                  <a class="bg-black text-white py-1.5 px-8 rounded-3xl ml-8 text-xl" href="#">Buy</a>
-                </div>
-            </div>
-            <div>
-              <img src="../assets/sneakersmain.png" alt="">
-            </div>
-         <i class="fa fa-angle-right my-auto ml-8 mr-24 cursor-pointer" style="font-size:42px"></i>
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
         </div>
-        <!-- Section 2 ends -->
-        <!-- Section 3 starts -->
-        <div>
-          <h1 class="text-left ml-32 text-4xl mt-32 mb-12">Lorem Ipsum</h1>
-          <!-- section 3 div1 -->
-            <div class="flex">
-                <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-               <a href="/login">
-                  <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                  <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-              </a>
-                </div>
-              <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-                <a href="/login">
-                <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-              </a>
-              </div>
-                <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-                  <a href="/login">
-                  <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                  <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-                  </a>
-                </div>
-            </div>
-          <!-- section 3 div2 -->
-                      <div class="flex mt-24">
-                <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-               <a href="/login">
-                  <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                  <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-              </a>
-                </div>
-              <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-                <a href="/login">
-                <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-              </a>
-              </div>
-                <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
-                  <a href="/login">
-                  <img id="child-image-hover" class="w-full cursor-pointer" src="../assets/sneakersmain.png" alt="oops">
-                  <p id="child-hover" class="text-white text-3xl text-semibold text-center -mt-32 pb-32">Lorem Ipsum Dolar.</p>
-                  </a>
-                </div>
-            </div>
-            <!-- section 3 div 2 ends -->
+      </div>
+      <!-- section 3 div2 -->
+      <div class="flex mt-24">
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
         </div>
-        <!-- section 3 ends -->
-        <!-- section 4 starts -->
-        <div>
-            <h1 class="text-left ml-32 text-4xl mt-32 mb-12">Lorem Ipsum</h1>
-          <div class="flex mx-72 justify-between">
-            <a href="/">
-              <div>
-                <img class="rounded-t-3xl" src="../assets/sneakersmain2.jpg" alt="alt">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Men</div>
-              </div>
-              </a>
-              <a href="/">
-              <div>
-                <img class="rounded-t-3xl" src="../assets/sneakersmain2.jpg" alt="alt">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Women</div>
-              </div>
-              </a>
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
+        </div>
+        <div id="parent-hover" class="w-1/3 mx-20 bg-transparent">
+          <a href="/login">
+            <img
+              id="child-image-hover"
+              class="w-full cursor-pointer"
+              src="../assets/sneakersmain.png"
+              alt="oops"
+            />
+            <p
+              id="child-hover"
+              class="text-white text-3xl text-semibold text-center -mt-32 pb-32"
+            >
+              Lorem Ipsum Dolar.
+            </p>
+          </a>
+        </div>
+      </div>
+      <!-- section 3 div 2 ends -->
+    </div>
+    <!-- section 3 ends -->
+    <!-- section 4 starts -->
+    <div>
+      <h1 class="text-left ml-32 text-4xl mt-32 mb-12">Lorem Ipsum</h1>
+      <div class="flex mx-72 justify-between">
+        <a href="/">
+          <div>
+            <img
+              class="rounded-t-3xl"
+              src="../assets/sneakersmain2.jpg"
+              alt="alt"
+            />
+            <div
+              style="
+                box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                  rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+              "
+              class="py-2 text-xl rounded-b-3xl"
+            >
+              Men
+            </div>
           </div>
-          <div class="flex mt-12 justify-center">
-              <div style="width: 17%" class="mx-2">
-                <img class="rounded-t-3xl"  src="../assets/sneakersmain2.jpg" alt="women">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Lorem Ipsum</div>
-              </div>
-              <div style="width: 17%" class="mx-2">
-                <img class="rounded-t-3xl"  src="../assets/sneakersmain2.jpg" alt="women">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Lorem Ipsum</div>
-              </div>
-              <div style="width: 17%" class="mx-2">
-                <img class="rounded-t-3xl"  src="../assets/sneakersmain2.jpg" alt="women">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Lorem Ipsum</div>
-              </div>
-              <div style="width: 17%" class="mx-2">
-                <img class="rounded-t-3xl" src="../assets/sneakersmain2.jpg" alt="women">
-                <div style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;" class="py-2 text-xl rounded-b-3xl">Lorem Ipsum</div>
-              </div>            
+        </a>
+        <a href="/">
+          <div>
+            <img
+              class="rounded-t-3xl"
+              src="../assets/sneakersmain2.jpg"
+              alt="alt"
+            />
+            <div
+              style="
+                box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                  rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+              "
+              class="py-2 text-xl rounded-b-3xl"
+            >
+              Women
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="flex mt-12 justify-center">
+        <div style="width: 17%" class="mx-2">
+          <img
+            class="rounded-t-3xl"
+            src="../assets/sneakersmain2.jpg"
+            alt="women"
+          />
+          <div
+            style="
+              box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+            "
+            class="py-2 text-xl rounded-b-3xl"
+          >
+            Lorem Ipsum
           </div>
         </div>
-    </div>    
+        <div style="width: 17%" class="mx-2">
+          <img
+            class="rounded-t-3xl"
+            src="../assets/sneakersmain2.jpg"
+            alt="women"
+          />
+          <div
+            style="
+              box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+            "
+            class="py-2 text-xl rounded-b-3xl"
+          >
+            Lorem Ipsum
+          </div>
+        </div>
+        <div style="width: 17%" class="mx-2">
+          <img
+            class="rounded-t-3xl"
+            src="../assets/sneakersmain2.jpg"
+            alt="women"
+          />
+          <div
+            style="
+              box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+            "
+            class="py-2 text-xl rounded-b-3xl"
+          >
+            Lorem Ipsum
+          </div>
+        </div>
+        <div style="width: 17%" class="mx-2">
+          <img
+            class="rounded-t-3xl"
+            src="../assets/sneakersmain2.jpg"
+            alt="women"
+          />
+          <div
+            style="
+              box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+                rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+            "
+            class="py-2 text-xl rounded-b-3xl"
+          >
+            Lorem Ipsum
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ProductSlideShow from "@/components/ProductSlideShow";
+import { throwStatement } from "@babel/types";
 export default {
   name: "HomeView",
-  components: {},
+  components: { ProductSlideShow },
+  data() {
+    return {
+      product: {},
+      products: [],
+      i: 0,
+    };
+  },
+  async mounted() {
+    const data = await fetch(`${this.$store.state.api}/products/getproducts`);
+    this.products = await data.json();
+    console.log(this.products);
+    this.product = this.products[0];
+  },
+  methods: {
+    async next() {
+      if (this.i < this.products.length - 1) {
+        this.i++;
+      } else {
+        this.i = 0;
+      }
+      this.product = this.products[this.i];
+    },
+    async prev() {
+      if (this.i > 0) {
+        this.i--;
+      } else {
+        this.i = this.products.length - 1;
+      }
+      this.product = this.products[this.i];
+    },
+  },
 };
 </script>
