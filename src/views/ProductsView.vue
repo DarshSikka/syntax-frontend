1fa98f1301835be52c1b5c8f1e2abd471c6736c9
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2022-08-12 23:15:40 +0530

    bug fixes in products

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -30,7 +30,7 @@
               id="color"
             >
               <option value="all">All</option>
-              <option value="orange">Orange</option>
+              <option value="white">White</option>
               <option value="red">Red</option>
               <option value="green">Green</option>
               <option value="blue">Blue</option>
@@ -124,7 +124,7 @@ export default {
       brand: "all",
       color: "all",
       category: "all",
-      gender: "",
+      gender: "B",
     };
   },
   async mounted() {
